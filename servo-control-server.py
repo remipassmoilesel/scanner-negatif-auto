42e7a70b9b150db9fd3591a8b879dd17dede51c8
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2018-01-04 12:45:34 +0100

    Improve http server, add git ignore

--- servo-control-server.py
+++ servo-control-server.py
@@ -1,14 +1,31 @@
-# See: https://docs.python.org/2/library/simplehttpserver.html
+#!/usr/bin/env python3
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-import SimpleHTTPServer
-import SocketServer
 
-PORT = 8000
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(b"<html><body><h1>hi!</h1></body></html>")
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+    def do_HEAD(self):
+        self._set_headers()
 
-print "serving at port", PORT
-httpd.serve_forever()
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write(b"<html><body><h1>POST!</h1></body></html>")
 
+
+def run(server_class=HTTPServer, handler_class=S):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
